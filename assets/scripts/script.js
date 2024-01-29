00e945d17c497a72062b5284298543c95b8cb273
hrach105 <hrachkhachatryan24@gmail.com>
2024-01-29 21:42:07 +0400

    changes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -423,37 +423,7 @@ canvas.onpointerout = (e) => {
 };
 
 
-;(function() {
-    const link = document.querySelectorAll('.hover-this')
-    const cursor = document.querySelector('.cursor')
-  
-    // * shake
-  
-    const animateit = function(e) {
-      const span = this.querySelector('h1')
-      const { offsetX: x, offsetY: y } = e,
-        { offsetWidth: width, offsetHeight: height } = this,
-        move = 25,
-        xMove = x / width * (move * 2) - move,
-        yMove = y / height * (move * 2) - move
-  
-      span.style.transform = `translate(${xMove}px, ${yMove}px)`
-  
-      if (e.type === 'mouseleave') span.style.transform = ''
-    }
-  
-    // * Cursor move
-  
-    const editCursor = (e) => {
-      const { clientX: x, clientY: y } = e
-      cursor.style.left = x + 'px'
-      cursor.style.top = y + 'px'
-    }
-  
-    link.forEach((b) => b.addEventListener('mousemove', animateit))
-    link.forEach((b) => b.addEventListener('mouseleave', animateit))
-    window.addEventListener('mousemove', editCursor)
-  })()
+
 
 // gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
